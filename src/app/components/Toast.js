a2e1ee326e2515b4e67837ad237a3dc00ab92359
Mukesh Sharma <83642159+Mukesh-Sharma400@users.noreply.github.com>
2024-10-05 17:12:19 +0530

    toast

--- src/app/components/Toast.js
+++ src/app/components/Toast.js
@@ -1,23 +1,46 @@
 import styled from "styled-components";
 
 export const Toast = ({ message }) => {
-  return <DisplayWrapper>{message}</DisplayWrapper>;
+  return (
+    <SmallNotchedBox>
+      <ContentWrapper>{message}</ContentWrapper>
+    </SmallNotchedBox>
+  );
 };
 
-const DisplayWrapper = styled.div`
+const SmallNotchedBox = styled.div`
+  width: 100%;
+  height: auto;
+  margin: 0 auto;
+  position: relative;
+  overflow: hidden;
+
+  &::before {
+    content: "";
+    position: absolute;
+    margin: -15px;
+    width: 30px;
+    height: 30px;
+    transform: rotate(45deg);
+    background-color: #00fa55;
+  }
+`;
+
+const ContentWrapper = styled.div`
   width: 400px;
   height: 50px;
-  border-radius: 10px;
   font-size: 16px;
   display: flex;
   align-items: center;
   justify-content: center;
   color: white;
-  background: rgba(0, 0, 0, 0.5);
-  box-shadow: 0 0px 20px rgba(0, 0, 0, 0.5);
-  backdrop-filter: blur(10.1px);
-  -webkit-backdrop-filter: blur(10.1px);
-  border: 1px solid rgba(0, 0, 0, 1);
+  background-color: #0f1c1e;
+  border: 1px solid
+    ${({ theme }) =>
+      theme.currentTheme === "dark"
+        ? theme.globalColors.darkGreenColor
+        : theme.globalColors.darkGreenColor};
+  clip-path: polygon(30px 0, 100% 0, 100% 100%, 100% 100%, 0 100%, 0 30px);
   transition: all 0.5s ease-in-out;
 
   @media (max-width: 400px) {
